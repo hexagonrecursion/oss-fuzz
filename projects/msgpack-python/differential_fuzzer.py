91ac658e2bfb26c1b435f4d74fbe926126fa7f73
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-03-31 16:03:19 +0300

    Real fuzz target

--- projects/msgpack-python/differential_fuzzer.py
+++ projects/msgpack-python/differential_fuzzer.py
@@ -1,10 +1,19 @@
 import sys
 
+from msgpack import _cmsgpack, fallback
+
 
 def TestOneInput(data):
-  if data == b"hello":
-    raise RuntimeError("world")
-
+    try:
+        from_extension = _cmsgpack.unpackb(buf)
+    except:
+        return
+    try:
+        from_fallback = fallback.unpackb(buf)
+    except:
+        return
+    if from_extension != from_fallback:
+        raise RuntimeError(f"Decoding disagreement: input: {data}, from extension: {from_extension}, from fallback: {from_fallback}")
 
 def main():
     import atheris
